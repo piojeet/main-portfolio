6c16893e45aa10a36bdb8bbb6f714cc5ba1087fe
piojeet <manjhipiojeet@gmail.com>
2025-01-22 23:30:56 +0530

    first commit

--- src/components/gpt/ChatForm.jsx
+++ src/components/gpt/ChatForm.jsx
@@ -1,8 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { IoMdArrowRoundUp } from 'react-icons/io';
 
 function ChatForm({ chatHistory, setChatHistory, geenrateBotResponse }) {
   const [inputValue, setInputValue] = useState('');
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    const handleFocus = () => {
+      inputRef.current.scrollIntoView({ behavior: 'smooth', block: 'nearest' });
+    };
+
+    const inputElement = inputRef.current;
+    inputElement.addEventListener('focus', handleFocus);
+
+    return () => {
+      inputElement.removeEventListener('focus', handleFocus);
+    };
+  }, []);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -23,6 +37,7 @@ function ChatForm({ chatHistory, setChatHistory, geenrateBotResponse }) {
       <form className="p-4" onSubmit={handleSubmit}>
         <div className="flex items-center outline-[1px] outline rounded-md px-2 focus-within:outline-[2px]">
           <input
+            ref={inputRef}
             type="text"
             className="w-full p-2 pl-0 outline-none"
             placeholder="Message..."
@@ -32,7 +47,7 @@ function ChatForm({ chatHistory, setChatHistory, geenrateBotResponse }) {
           />
           <button
             type="submit"
-            className={`p-2  bg-black transition-all duration-200 ${inputValue.trim() ? 'scale-100 rounded-md text-white pointer-events-auto' : 'scale-50 rounded-full text-black pointer-events-none rotate-180'}`}
+            className={`p-2 bg-black transition-all duration-200 ${inputValue.trim() ? 'scale-100 rounded-md text-white pointer-events-auto' : 'scale-50 rounded-full text-black pointer-events-none rotate-180'}`}
           >
             <IoMdArrowRoundUp />
           </button>
